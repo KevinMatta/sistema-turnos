359eb03b6ae588200acad70f942d82af0e5b152a
kevin matta <kmembreno@unah.hn>
2024-04-09 16:27:09 -0600

    feat(app): validaciones create calendario

--- app/Sistema_Turnos/Controllers/TurnosPorEmpleadoController.cs
+++ app/Sistema_Turnos/Controllers/TurnosPorEmpleadoController.cs
@@ -46,15 +46,66 @@ namespace Sistema_Turnos.Controllers
         {
             try
             {
-                item.TuEm_Creacion = 1;
-                item.TuEm_FechaCreacion = DateTime.Now;
-                var list = await _turnosPorEmpleadoService.CrearTurnoEmpleado(item);
-                string[] notificaciones = new string[4];
-                notificaciones[0] = "tim-icons icon-alert-circle-exc";
-                notificaciones[1] = "Agregado";
-                notificaciones[2] = "Se agregaron los datos con exito";
-                notificaciones[3] = "info";
-                TempData["Notificaciones"] = notificaciones;
+                var listaTurnosEmpleado = new List<TurnosPorEmpleadoViewModel>();
+                var listaTurnosEmpleadoResponse = await _turnosPorEmpleadoService.TurnosPorEmpleadoList();
+                listaTurnosEmpleado = (List<TurnosPorEmpleadoViewModel>)listaTurnosEmpleadoResponse.Data;
+                DateTime fechaSeleccionada = item.TuEm_FechaInicio;
+
+                var mismoDia = listaTurnosEmpleado.Where(t => t.TuEm_FechaInicio.Date == fechaSeleccionada).ToList();
+
+                var turnoNocturno = listaTurnosEmpleado.Where(t =>
+                    (t.TuEm_FechaInicio.Date == fechaSeleccionada.AddDays(-1) ||
+                    t.TuEm_FechaInicio.Date == fechaSeleccionada.AddDays(1)) &&
+                    t.Turn_Id == 3).ToList();
+
+                var semanaPasada = listaTurnosEmpleado.Where(t =>
+                    (t.TuEm_FechaInicio.Date == fechaSeleccionada.AddDays(-7) ||
+                    t.TuEm_FechaInicio.Date == fechaSeleccionada.AddDays(7)) &&
+                    t.Turn_Id == item.Turn_Id).ToList();
+
+                if (semanaPasada.Any(t => t.Empl_Id == item.Empl_Id))
+                {
+                    string[] notificaciones = new string[4];
+                    notificaciones[0] = "tim-icons icon-bell-55";
+                    notificaciones[1] = "Advertencia";
+                    notificaciones[2] = "Este empleado ya tuvo este turno la semana pasada";
+                    notificaciones[3] = "warning";
+                    TempData["Notificaciones"] = notificaciones;
+                }
+
+                if (mismoDia.Any(t => t.Empl_Id == item.Empl_Id))
+                {
+                    string[] notificaciones = new string[4];
+                    notificaciones[0] = "tim-icons icon-bell-55";
+                    notificaciones[1] = "Advertencia";
+                    notificaciones[2] = "Este empleado ya tiene un turno para este dia";
+                    notificaciones[3] = "warning";
+                    TempData["Notificaciones"] = notificaciones;
+                }
+
+                if (turnoNocturno.Any(t => t.Empl_Id == item.Empl_Id))
+                {
+                    string[] notificaciones = new string[4];
+                    notificaciones[0] = "tim-icons icon-bell-55";
+                    notificaciones[1] = "Advertencia";
+                    notificaciones[2] = "No puede tener este turno 2 dias seguidos";
+                    notificaciones[3] = "warning";
+                    TempData["Notificaciones"] = notificaciones;
+                }
+
+                if (false)
+                {
+                    //listo para insertar
+                    item.TuEm_Creacion = 1;
+                    item.TuEm_FechaCreacion = DateTime.Now;
+                    var list = await _turnosPorEmpleadoService.CrearTurnoEmpleado(item);
+                    string[] notificaciones = new string[4];
+                    notificaciones[0] = "tim-icons icon-alert-circle-exc";
+                    notificaciones[1] = "Agregado";
+                    notificaciones[2] = "Se agregaron los datos con exito";
+                    notificaciones[3] = "info";
+                    TempData["Notificaciones"] = notificaciones;
+                }
 
                 return RedirectToAction("Index", "TurnosPorEmpleado");
             }
